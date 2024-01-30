21cfb82787b01f1719cceed4a2abbdf70ea8ad92
veltanmarius <148964546+veltanmarius@users.noreply.github.com>
2024-01-30 10:06:43 +0200

    format exceptions not to expose system errors

--- src/main/java/ro/veltanmarius/mkcustomer/rest/controller/GlobalExceptionHandler.java
+++ src/main/java/ro/veltanmarius/mkcustomer/rest/controller/GlobalExceptionHandler.java
@@ -1,18 +1,19 @@
 package ro.veltanmarius.mkcustomer.rest.controller;
-import static org.springframework.http.HttpStatus.NOT_FOUND;
-import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.support.WebExchangeBindException;
 import ro.veltanmarius.mkcustomer.exceptions.InvalidInputException;
 import ro.veltanmarius.mkcustomer.exceptions.ObjectNotFoundException;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.*;
 
 /**
  * @author Marius Veltan
@@ -26,7 +27,7 @@ public final class GlobalExceptionHandler {
     public @ResponseBody GlobalHttpErrorInfo handleNotFoundExceptions(
             ServerHttpRequest request, ObjectNotFoundException ex) {
 
-        return createHttpErrorInfo(NOT_FOUND, request, ex);
+        return createHttpErrorInfo(NOT_FOUND, request, ex, null);
     }
 
     @ResponseStatus(UNPROCESSABLE_ENTITY)
@@ -34,14 +35,45 @@ public final class GlobalExceptionHandler {
     public @ResponseBody GlobalHttpErrorInfo handleInvalidInputException(
             ServerHttpRequest request, InvalidInputException ex) {
 
-        return createHttpErrorInfo(UNPROCESSABLE_ENTITY, request, ex);
+        return createHttpErrorInfo(UNPROCESSABLE_ENTITY, request, ex, null);
+    }
+
+    @ResponseStatus(BAD_REQUEST)
+    @ExceptionHandler(WebExchangeBindException.class)
+    public @ResponseBody GlobalHttpErrorInfo handleBindErrors(
+            ServerHttpRequest request, WebExchangeBindException ex) {
+        log.debug("handleBindErrors start");
+        List<String> errors = ex.getBindingResult().getFieldErrors()
+                .stream().map(e -> "Field " + e.getField() + " - " + e.getDefaultMessage()).toList();
+        return createHttpErrorInfo(BAD_REQUEST, request, ex, errors);
+    }
+
+    @ResponseStatus(INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(RuntimeException.class)
+    public @ResponseBody GlobalHttpErrorInfo handleRuntimeExceptions(
+            ServerHttpRequest request, RuntimeException ex) {
+        log.debug("handleRuntimeExceptions start");
+        return createHttpErrorInfo(INTERNAL_SERVER_ERROR, request, ex, null);
+    }
+
+    @ResponseStatus(INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public @ResponseBody GlobalHttpErrorInfo handleGeneralExceptions(
+            ServerHttpRequest request, Exception ex) {
+        log.debug("handleGeneralExceptions start");
+        return createHttpErrorInfo(INTERNAL_SERVER_ERROR, request, ex, null);
     }
 
     private GlobalHttpErrorInfo createHttpErrorInfo(
-            HttpStatus httpStatus, ServerHttpRequest request, Exception ex) {
+            HttpStatus httpStatus, ServerHttpRequest request, Exception ex, List<String> errors) {
 
         final String path = request.getPath().pathWithinApplication().value();
-        final String message = ex.getMessage();
+        final String message;
+        if (errors == null) {
+            message = ex.getMessage();
+        } else {
+            message = String.join(" | ", errors);
+        }
 
         log.debug("Returning HTTP status: {} for path: {}, message: {}", httpStatus, path, message);
         return new GlobalHttpErrorInfo(httpStatus, path, message);
